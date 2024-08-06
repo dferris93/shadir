ff5dde0eec799698b595cfcbac0c2098d8acf45c
Dan Ferris <dan@usrsbin.com>
2024-08-06 08:20:58 -0700

    updated help msg

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	    "context"
-        "crypto/md5"
-        "crypto/sha1"
-        "crypto/sha256"
-        "crypto/sha512"
-        "flag"
-        "fmt"
-        "hash"
-        "hash/crc32"
-        "hash/crc64"
-        "io"
-        "io/fs"
-        "os"
-        "path/filepath"
-        "regexp"
-        "sync"
-        "syscall"
+	"context"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"hash"
+	"hash/crc32"
+	"hash/crc64"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sync"
+	"syscall"
 
-        "golang.org/x/crypto/blake2b"
-		"github.com/jzelinskie/whirlpool"
-        "golang.org/x/crypto/sha3"
-        "golang.org/x/sync/errgroup"
+	"github.com/jzelinskie/whirlpool"
+	"golang.org/x/crypto/blake2b"
+	"golang.org/x/crypto/sha3"
+	"golang.org/x/sync/errgroup"
 )
 
 func worker(path string, hardLinks *sync.Map, info fs.FileInfo, hashalgo string) error {
@@ -35,37 +35,37 @@ func worker(path string, hardLinks *sync.Map, info fs.FileInfo, hashalgo string)
 			fmt.Printf("%s  %s  *\n", h, path)
 			return nil
 		}
-	} 
+	}
 
 	var hasher hash.Hash
 
 	switch hashalgo {
-        case "crc32":
-            hasher = crc32.NewIEEE()
-        case "crc64":
-            hasher = crc64.New(crc64.MakeTable(crc64.ISO))
-        case "md5":
-            hasher = md5.New()
-        case "sha1":
-            hasher = sha1.New()
-        case "sha256":
-            hasher = sha256.New()
-        case "sha512":
-            hasher = sha512.New()
-        case "sha3-512":
-            hasher = sha3.New512()
-        case "sha3-256":
-            hasher = sha3.New256()
-        case "blake2b-512":
-            hasher, _ = blake2b.New512(nil)
-        case "blake2b-256":
-            hasher, _ = blake2b.New256(nil)
-		case "whirlpool":
-			hasher = whirlpool.New()
-        default:
-            fmt.Fprintf(os.Stderr, "unsupported hash algorithm %s falling back to sha256", hashalgo)
-            hasher = sha256.New()
-    }
+	case "crc32":
+		hasher = crc32.NewIEEE()
+	case "crc64":
+		hasher = crc64.New(crc64.MakeTable(crc64.ISO))
+	case "md5":
+		hasher = md5.New()
+	case "sha1":
+		hasher = sha1.New()
+	case "sha256":
+		hasher = sha256.New()
+	case "sha512":
+		hasher = sha512.New()
+	case "sha3-512":
+		hasher = sha3.New512()
+	case "sha3-256":
+		hasher = sha3.New256()
+	case "blake2b-512":
+		hasher, _ = blake2b.New512(nil)
+	case "blake2b-256":
+		hasher, _ = blake2b.New256(nil)
+	case "whirlpool":
+		hasher = whirlpool.New()
+	default:
+		fmt.Fprintf(os.Stderr, "unsupported hash algorithm %s falling back to sha256", hashalgo)
+		hasher = sha256.New()
+	}
 
 	file, err := os.Open(path)
 	if err != nil {
@@ -102,75 +102,74 @@ func checkSymlink(path string) (bool, string, error) {
 }
 
 func walkDirectory(dir string, eg *errgroup.Group, followSymlinks bool, hashalgo string, excludePattern *regexp.Regexp) uint64 {
-    hardLinks := new(sync.Map)
-    filecount := uint64(0)
+	hardLinks := new(sync.Map)
+	filecount := uint64(0)
 
-    filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "unable to walk %s: %v\n", path, err)
-            return nil
-        }
-        if info.IsDir() {
-            if excludePattern != nil && excludePattern.MatchString(path) {
-                return filepath.SkipDir
-            } else {
-                return nil 
-            }
-        }
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to walk %s: %v\n", path, err)
+			return nil
+		}
+		if info.IsDir() {
+			if excludePattern != nil && excludePattern.MatchString(path) {
+				return filepath.SkipDir
+			} else {
+				return nil
+			}
+		}
 
-        if excludePattern != nil && excludePattern.MatchString(path) {
-            return nil
-        }
+		if excludePattern != nil && excludePattern.MatchString(path) {
+			return nil
+		}
 
-        if info.Mode().IsRegular() {
-            filecount++
-            eg.Go(func() error {
-                return worker(path, hardLinks, info, hashalgo)
-            })
-            return nil
-        }
+		if info.Mode().IsRegular() {
+			filecount++
+			eg.Go(func() error {
+				return worker(path, hardLinks, info, hashalgo)
+			})
+			return nil
+		}
 
-        if followSymlinks {
-            isSymlink, target, err := checkSymlink(path)
-            if err != nil {
-                fmt.Fprintf(os.Stderr, "error reading symlink %s: %v\n", path, err)
-                return nil
-            }
-            if isSymlink {
-                filecount++
-                eg.Go(func() error {
-                    return worker(target, hardLinks, info, hashalgo)
-                })
-            }
-        } else {
-            fmt.Fprintf(os.Stderr, "skipping non-regular file %s\n", path)
-        }
-        return nil
-    })
-    return filecount
+		if followSymlinks {
+			isSymlink, target, err := checkSymlink(path)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error reading symlink %s: %v\n", path, err)
+				return nil
+			}
+			if isSymlink {
+				filecount++
+				eg.Go(func() error {
+					return worker(target, hardLinks, info, hashalgo)
+				})
+			}
+		} else {
+			fmt.Fprintf(os.Stderr, "skipping non-regular file %s\n", path)
+		}
+		return nil
+	})
+	return filecount
 }
-
 
 func main() {
 	dir := flag.String("dir", ".", "directory to process")
 	poolSize := flag.Int("poolsize", 8, "number of workers")
 	followSymlinks := flag.Bool("follow-symlinks", false, "follow symlinks")
-	hashalgo := flag.String("hash", "sha256", "hash algorithm")
-    exclude := flag.String("exclude", "", "exclude files matching this regex pattern")
+	hashalgo := flag.String("hash", "sha256", "hash algorithm.  Choices are crc32, crc64, md5, sha1, sha256, sha512, sha3-512, sha3-256, blake2b-512, blake2b-256, whirlpool")
+	exclude := flag.String("exclude", "", "exclude files matching this regex pattern")
 	flag.Parse()
 
-    var excludePattern *regexp.Regexp
-    var err error
+	var excludePattern *regexp.Regexp
+	var err error
 
-    if *exclude != "" {
-        excludePattern, err = regexp.Compile(*exclude)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "invalid exclude pattern: %v\n", err)
-            os.Exit(1)
-        }
-    } else {
-        excludePattern = nil
-    }
+	if *exclude != "" {
+		excludePattern, err = regexp.Compile(*exclude)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "invalid exclude pattern: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		excludePattern = nil
+	}
 
 	eg, _ := errgroup.WithContext(context.Background())
 
@@ -178,11 +177,11 @@ func main() {
 		eg.SetLimit(*poolSize)
 	}
 
-    absPath, err := filepath.Abs(filepath.Clean(*dir))
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-        os.Exit(1)
-    }
+	absPath, err := filepath.Abs(filepath.Clean(*dir))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 	filecount := walkDirectory(absPath, eg, *followSymlinks, *hashalgo, excludePattern)
 
 	err = eg.Wait()
